bc4f80bc4e34223cb10ec2cf4cbdb2f1cec1ddfd
has12zen <57583693+has12zen@users.noreply.github.com>
2021-01-15 11:17:07 +0530

    added axios

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -13,20 +14,7 @@ app.use((req, res, next) => {
 	next();
 });
 //TODO:
-app.use('/', async (req, res) => {
-	videoUrlLink.twitter.getInfo(req.query.url, {}, (error, info) => {
-		if (error) {
-			console.error(error);
-		} else {
-			console.log(info.full_text);
-			console.log(info.variants);
-			res.status(200).json({
-				status: 'success',
-				data: info,
-			});
-		}
-	});
-});
+app.use('/', async (req, res) => {});
 app.all('*', (req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
